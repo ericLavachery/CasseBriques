243090efbdfa1f958f12331e7c5d167516d4b86b
ericLavachery <eric.lavachery@gmail.com>
2018-09-06 11:50:33 +0200

    hard bricks, require 2 hits

--- cb.js
+++ cb.js
@@ -1,9 +1,10 @@
-var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
+var game = new Phaser.Game(640, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
 
 var ball;
 var paddle;
 var bricks;
 var metals;
+var hardbricks;
 var newBrick;
 var brickInfo;
 var scoreText;
@@ -27,6 +28,7 @@ function preload() {
     game.load.image('paddle', 'img/paddle.png');
     game.load.image('brick', 'img/brick.png');
     game.load.image('metal', 'img/metal.png');
+    game.load.image('hardbrick', 'img/hard.png');
     game.load.spritesheet('ball', 'img/wobble.png', 20, 20);
     // game.load.spritesheet('button', 'img/button.png', 120, 40);
 }
@@ -48,12 +50,13 @@ function create() {
     paddle.anchor.set(0.5,1);
     game.physics.enable(paddle, Phaser.Physics.ARCADE);
     paddle.body.immovable = true;
-    initBricks();
+    // initBricks();
+    initLevel();
     textStyle = { font: '18px Arial', fill: '#0095DD' };
     scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
     livesText = game.add.text(game.world.width-5, 5, 'Lives: '+lives, textStyle);
     livesText.anchor.set(1,0);
-    lifeLostText = game.add.text(game.world.width*0.5, game.world.height*0.5, 'Life lost, click to continue', textStyle);
+    lifeLostText = game.add.text(game.world.width*0.5, game.world.height*0.6, 'Life lost, click to continue', textStyle);
     lifeLostText.anchor.set(0.5);
     lifeLostText.visible = false;
     // startButton = game.add.button(game.world.width*0.5, game.world.height*0.5, 'button', startGame, this, 1, 0, 2);
@@ -63,6 +66,7 @@ function update() {
     game.physics.arcade.collide(ball, paddle, ballHitPaddle);
     game.physics.arcade.collide(ball, bricks, ballHitBrick);
     game.physics.arcade.collide(ball, metals, ballHitMetal);
+    game.physics.arcade.collide(ball, hardbricks, ballHitHardbrick);
     paddle.x = game.input.x || game.world.width*0.5;
     // if(playing) {
     //     paddle.x = game.input.x || game.world.width*0.5;
@@ -74,16 +78,17 @@ function initBricks() {
         height: 30,
         count: {
             row: 3,
-            col: 7
+            col: 9
         },
         offset: {
             top: 45,
             left: 60
         },
-        padding: 10
+        padding: 15
     }
     bricks = game.add.group();
     metals = game.add.group();
+    hardbricks = game.add.group();
     for(c=0; c<brickInfo.count.col; c++) {
         for(r=0; r<brickInfo.count.row; r++) {
             var brickX = (c*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
@@ -114,6 +119,61 @@ function initBricks() {
         }
     }
 }
+function initLevel() {
+    brickInfo = {
+        width: 50,
+        height: 30,
+        count: {
+            row: 3,
+            col: 9
+        },
+        offset: {
+            top: 45,
+            left: 60
+        },
+        padding: 15
+    }
+    bnum = 0;
+    bricks = game.add.group();
+    metals = game.add.group();
+    hardbricks = game.add.group();
+    for(c=0; c<brickInfo.count.col; c++) {
+        for(r=0; r<brickInfo.count.row; r++) {
+            bnum++;
+            var brickX = (c*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
+            var brickY = (r*(brickInfo.height+brickInfo.padding))+brickInfo.offset.top;
+            if (bnum == 1 || bnum == 5 || bnum == 9 || bnum == 11 || bnum == 13 || bnum == 17 || bnum == 21 || bnum == 23 || bnum == 25) {
+                newBrick = game.add.sprite(brickX, brickY, 'metal');
+                game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+                newBrick.body.immovable = true;
+                newBrick.anchor.set(0.5);
+                metals.add(newBrick);
+                brickValue += brickBonus;
+                if (brickValue < 32) {
+                    brickBonus += 1;
+                } else if (brickValue < 54) {
+                    brickBonus += 2;
+                } else if (brickValue < 98) {
+                    brickBonus += 4;
+                } else {
+                    brickBonus += 6;
+                }
+            } else if (bnum == 3 || bnum == 6 || bnum == 12 || bnum == 15 || bnum == 18 || bnum == 24 || bnum == 27) {
+                newBrick = game.add.sprite(brickX, brickY, 'hardbrick');
+                game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+                newBrick.body.immovable = true;
+                newBrick.anchor.set(0.5);
+                hardbricks.add(newBrick);
+            } else {
+                newBrick = game.add.sprite(brickX, brickY, 'brick');
+                game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+                newBrick.body.immovable = true;
+                newBrick.anchor.set(0.5);
+                bricks.add(newBrick);
+            }
+        }
+    }
+}
 function ballHitBrick(ball, brick) {
     var killTween = game.add.tween(brick.scale);
     killTween.to({x:0,y:0}, 200, Phaser.Easing.Linear.None);
@@ -133,6 +193,18 @@ function ballHitBrick(ball, brick) {
         alert('You won the game, congratulations!');
         location.reload();
     }
+}
+function ballHitHardbrick(ball, hardbrick) {
+    hardbrick.kill();
+    score += brickValue;
+    scoreText.setText('Points: '+score);
+    brickX = hardbrick.world.x;
+    brickY = hardbrick.world.y;
+    newBrick = game.add.sprite(brickX, brickY, 'brick');
+    game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+    newBrick.body.immovable = true;
+    newBrick.anchor.set(0.5);
+    bricks.add(newBrick);
 }
 function ballHitMetal(ball, metal) {
     score -= 1;
